25aed859724393cf692ff8a1c52b35c26dc9d572
waicharikim <waicharikimani@gmail.com>
2022-04-07 19:39:27 -0700

    task 5 task 6 fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
 *main entry point
-*hello_world printf task
-*return 1 after program runs
+*hello world printf task
+*Return: return 1 after program runs
 */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+ printf("with proper grammar, but the outcome is a piece of art,\n");
+ return (0);
 }
