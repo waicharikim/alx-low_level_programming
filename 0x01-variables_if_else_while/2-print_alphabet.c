45aff32789c6f703ad1001863c199db7310d1799
waicharikim <waicharikimani@gmail.com>
2022-04-08 17:45:37 -0700

    task 2 print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Description: print the lowercase alphabet
+*Return: 0
+*/
+int main(void)
+{
+int n;
+
+for (c = 0; c < 26;++c)
+putchar('a' + c);
+
+putchar('\n');
+
+return (0);
+}
