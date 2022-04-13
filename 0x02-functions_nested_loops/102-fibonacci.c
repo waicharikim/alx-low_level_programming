c7ee16bfe75de8ed4a08223cd21d6497b0603a3f
waicharikim <waicharikimani@gmail.com>
2022-04-12 20:17:32 -0700

    task 12

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*add - adds two numbers
+*@i: the first number
+*@k: the second number
+*
+*Return: The result of the addition
+*/
+int add(int i, int k)
+{
+return (i + k);
+}
