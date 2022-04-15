721626873a544846da4783c80025d95af52d0ec3
waicharikim <waicharikimani@gmail.com>
2022-04-14 18:43:42 -0700

    task 10 fix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,22 +12,22 @@ int i, k, x, y;
 if (size > 0)
 {
 i = size - 1;
-for (x = 0; x < size; x++)
+for (x = 0; x < size ; x++)
 {
-for (y = i; y > 0; y--)
+for (y = i; y > 0 ; y--)
 {
 _putchar(' ');
 }
 for (k = 0; k <= x; k++)
 {
-_putchar(35);
+_putchar (35);
 }
 i--;
-_putcar('\n');
+_putchar ('\n');
 }
 }
 else
 {
-_putchar('\n');
+_putchar ('\n');
 }
 }
