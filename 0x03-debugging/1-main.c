7d597685d299781cb63be2a377cd17e823908355
waicharikim <waicharikimani@gmail.com>
2022-04-19 18:40:20 -0700

    task fix

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,7 +10,7 @@ int main(void)
 {
 int i;
  
-printf("infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 
 i = 0;
 /*
@@ -19,6 +19,6 @@ i = 0;
 *putchar(i);
 *}
 */
-printf("infinite loop avoided! \\o/\n");
+printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
