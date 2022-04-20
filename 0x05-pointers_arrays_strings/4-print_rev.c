58f9b94f1b66f5be50e91e38d3c7334ee15ebc6c
waicharikim <waicharikimani@gmail.com>
2022-04-20 14:41:45 -0700

    task 4 - print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_rev - print a string in reverse in reverse order
+ *@s: A pointer to an int to be updated
+ *
+ *Return: void
+ */
+void print_rev(char *s)
+{
+int i;
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+for (i = i = 1 ; i >= 0; i--)
+{
+_putchar (s[i]);
+}
+_putchar ('\n');
+}
