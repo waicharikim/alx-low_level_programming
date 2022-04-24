779d7ed8a82a7b5f36b38c1e220feeaf2a22d35b
waicharikim <waicharikimani@gmail.com>
2022-04-24 13:19:18 -0700

    task 1 - concantenating 2 strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *_strncat - concantenates 2 strings
+ *@dest: a pointer to a character to be modified
+ *@src: pointer to a character to be modified
+ *@n: value
+ *return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j;
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
