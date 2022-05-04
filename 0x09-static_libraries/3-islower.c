8e0c5b2d5a68100379f60a052e4c996cda1fc840
waicharikim <waicharikimani@gmail.com>
2022-05-03 18:23:48 -0700

    static libraries tasks fix

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*_islower - checks if a character is lowercase
+*@c: contains value to be compared
+*Return:Always 0
+*/
+int _islower(int c)
+{
+
+if (c > 'a' && c < 'z')
+{  
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
