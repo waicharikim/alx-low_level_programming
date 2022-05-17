3db953c4204a6ea431363ab07f472ac091ac6e8f
waicharikim <waicharikimani@gmail.com>
2022-05-16 18:17:50 -0700

    function pointers fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -30,8 +30,8 @@ void print_name_uppercase(char *name)
 }
 int main(void)
 {
-  print_name("Danton", print_name_as_is);
-  print_name("Rick Harris", print_name_uppercase);
+  print_name("Bob", print_name_as_is);
+  print_name("Bob Dylan", print_name_uppercase);
 printf("\n");
  return(0);
 }
