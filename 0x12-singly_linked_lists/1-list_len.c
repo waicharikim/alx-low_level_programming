008990818c5026373e439ae5a4e4af44d2345d50
waicharikim <waicharikimani@gmail.com>
2022-05-24 15:48:48 -0700

    fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,10 +7,10 @@
  */
 size_t list_len(const list_t *h)
 {
-  unsigned int nodes = 0;
-  while (h){
-    nodes++;
+  size_t nodes = 0;
+  while (h != NULL){
     h = h->next;
+    nodes++;
   }
   return (nodes)
     }
