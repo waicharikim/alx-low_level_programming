a3429214c99ae3eb9fb27e95904b1fb3061b9c99
waicharikim <waicharikimani@gmail.com>
2022-05-27 17:33:07 -0700

    fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,12 +8,12 @@
 */
 size_t print_listint(const listint_t *h)
 {
-size_t count = 0;
-while(h)
+size_t i;
+for (i = 0; h; i++)
 {
 printf("%d\n", h->n);
 count++;
 h = h->next;
 }
-return (count);
+return (i);
 }
