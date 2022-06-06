4b75e311e2b7e835d4fecc7a4c96f262ee059759
Waichari <101750843+waicharikim@users.noreply.github.com>
2022-06-07 02:13:05 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@ int create_file(const char *filename, char *text_content)
 int file, fwrite, i;
 if (filename == NULL)
 return (-1);
-file = open(filename, O-CREAT | O_TRUNC | O_WRONLY, 0600);
+file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 if (file == -1)
 return (-1);
 if (text_content != NULL)
