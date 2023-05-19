c630d2c1ec63efa9213be986ed08261d26864eb2
waicharikim <waicharikimani@gmail.com>
2023-05-19 12:41:47 +0300

    print to stderr
    fprintf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+/**
+ * main - program entry
+ *
+ * Description: print to stderr using fprintf
+ * Return: 1 (stderr)
+*/
+#include <stdio.h>
+
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
