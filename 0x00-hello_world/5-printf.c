49f51735387d79c94efe0cb102efea362d3a28cf
waicharikim <waicharikimani@gmail.com>
2023-05-19 11:22:55 +0300

    print a string of charsusing  printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+ * main - prints a string of characters
+ *
+ * Description: prints a string pf char and appends a new line
+ * Return: 0 (sucess)
+*/
+#include <stdio.h>
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n" );
+return (0);
+}
