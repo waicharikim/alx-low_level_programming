266ca2a0ed5a6054a426f7a75e8b6e8090a90561
waicharikim <waicharikimani@gmail.com>
2023-05-20 02:01:57 +0300

    digits
    print all single decimal digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program entry
+ *
+ * Description: prints all single decimal digits
+ * Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%i", n);
+	printf("\n");
+	return (0);
+}
