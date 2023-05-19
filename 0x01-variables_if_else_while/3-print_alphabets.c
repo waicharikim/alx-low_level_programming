79680775a1742027ae1b651f52bcdc3d2b4e6cc7
waicharikim <waicharikimani@gmail.com>
2023-05-20 01:32:35 +0300

    alphabets v2
    print lowercase and  UPPERCASE alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - program entry
+ *
+ * Description: print lowercase and UPPERCASE alphabets
+ * Return: 0
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
