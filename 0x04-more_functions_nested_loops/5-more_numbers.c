f066f0bbfc9f52230a41d59e339db38bfcc5dd25
waicharikim <waicharikimani@gmail.com>
2023-05-26 01:14:26 +0300

    more numbers fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,17 +7,21 @@
  */
 void more_numbers(void)
 {
+	int c;
 	int n;
 	int n1;
 	int n2;
 
-	for (n = 0; n < 15; n++)
+	for (c = 0; c < 10; c++)
 	{
-		n1 = n / 10;
-		n2 = n % 10;
-		if (n1 != 0)
-		_putchar(n1 + 48);
-		_putchar(n2 + 48);
+		for (n = 0; n < 15; n++)
+		{
+			n1 = n / 10;
+			n2 = n % 10;
+			if (n1 != 0)
+				_putchar(n1 + 48);
+			_putchar(n2 + 48);
+		}
+		_putchar(10);
 	}
-	_putchar(10);
 }
