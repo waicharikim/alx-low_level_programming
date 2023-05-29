6efc27c4acdb3e5518bd5a9cc27f29978cb77a72
waicharikim <waicharikimani@gmail.com>
2023-05-29 10:10:02 +0300

    update pointer content
    update pointer to an int to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - update the value of *n to 98
+ * @*n: pointer to the value to be updated
+ *
+ * return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
