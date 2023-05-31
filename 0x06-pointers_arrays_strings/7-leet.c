8a44e18d5c0cc90491a6b9e373a2c863ad2cd16a
waicharikim <waicharikimani@gmail.com>
2023-06-01 00:24:32 +0300

    encode 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * leet - encode a string into 1337
+ * @str: pointer to string
+ *
+ * Return: pointer to encoded string
+ */
+
+char *leet(char *str)
+{
+	char *tmp;
+
+	tmp = str;
+	while (*tmp)
+	{
+	}
+}
