c53bb7aa7542cb9be0e7a9d09a9f88ec1f94dc97
waicharikim <waicharikimani@gmail.com>
2023-06-13 23:20:26 +0300

    betty fix & handled size < 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,15 +13,17 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
-		ptr = malloc(sizeof(char) * size);
-		if (ptr != NULL)
+	if (size < 1)
+		return (NULL);
+	ptr = malloc(sizeof(char) * size);
+	if (ptr != NULL)
+	{
+		for (i = 0; i < size; i++)
 		{
-			for (i = 0; i < size; i++)
-			{
-				ptr[i] = c;	
-			}
-			return (ptr);
+			ptr[i] = c;
 		}
-		else
-			return (NULL);
+		return (ptr);
+	}
+	else
+		return (NULL);
 }
