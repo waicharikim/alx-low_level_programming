d0afe5dc0d82f8a2b00abc0e8fdf16cd41feb409
waicharikim <waicharikimani@gmail.com>
2023-06-13 23:28:30 +0300

    handled null passed to str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,6 +17,8 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	int j;
 
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
 	for (len = s2, count = 0; *len; count++, len++)
 		;
 	for (dest = s1; *dest; count++, dest++)
