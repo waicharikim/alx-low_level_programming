edc3c82061314b06800fb7e3e2d2a735aced148e
waicharikim <waicharikimani@gmail.com>
2023-06-15 23:11:51 +0300

    initiliased allocated memory to 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
-		return (NULL);
-	return (ptr);
+	if (ptr != NULL)
+		for (i = 0; nmemb; i++,nmemb--)
+		{
+			*(ptr + 0) = 0;
+			return (ptr);
+		}
+	return (NULL);
 }
