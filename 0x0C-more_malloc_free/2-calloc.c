ff34b8655330f35081cfca3e5897b66f169d716a
waicharikim <waicharikimani@gmail.com>
2023-06-16 10:15:02 +0300

    initialised allocated memory to zero
    nmemb * size

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,17 +12,15 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
 	unsigned int i;
-	char *c;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (ptr != NULL)
 	{
-		c = (char *)ptr;
 		for (i = 0; i < nmemb * size; i++)
 		{
-			*(c + i) = '\0';
+			*((char *)ptr + i) = '\0';
 		}
 		return (ptr);
 	}
