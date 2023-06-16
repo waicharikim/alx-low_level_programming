0f31e0c9ba092b75b0149147c1e384db5bd7f4bf
waicharikim <waicharikimani@gmail.com>
2023-06-16 13:09:22 +0300

    handling null passed argument

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,15 +17,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i;
 
 	len = 0;
-	for (src = s2; *src; src++, len++)
-		;
 	if (!s1)
-		s1 = NULL;
+		s1 = "";
 	if (!s2)
 	{
-		s2 = NULL;
-		n = 0;
+		s2 = "";
 	}
+	for (src = s2; *src; src++, len++)
+		;
 	if (n > len)
 		n = len;
 	str = malloc(sizeof(s1) + (sizeof(char) * n));
