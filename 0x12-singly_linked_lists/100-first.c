f9aad4baa9bd573394fca2e2089d1df3bb8a3f30
waicharikim <waicharikimani@gmail.com>
2023-07-05 15:26:03 +0300

    print first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * print_first - prints a string before main
+ *
+ * Return: void
+ */
+void print_first(void)__attribute__((constructor));
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
