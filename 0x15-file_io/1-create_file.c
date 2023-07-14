2e7745db7073604c68d52ee46f4bcb59b702bb3d
waicharikim <waicharikimani@gmail.com>
2023-07-14 15:38:56 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/**
+11;rgb:0000/0000/0000/**
  * create_file - create a file
  * @filename: name of the fileto create
  * @text_content: string to write to file
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		;
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_RDWR | O_TRUNC, S_IRUSR, S_IWUSR);
+	fd = open(filename,O_CREAT | O_RDWR | O_TRUNC, S_IRUSR, S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (write(fd, text_content, count) == -1)
