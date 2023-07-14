1aab1018feb245d3483ebda848b7d5febcc77777
waicharikim <waicharikimani@gmail.com>
2023-07-14 16:10:35 -0700

    bug fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,15 +13,16 @@ int create_file(const char *filename, char *text_content)
 	char *counter;
 
 	count = 0;
-	for (counter = text_content; *counter; count++, counter++)
-		;
+	if (text_content)
+	{
+		for (counter = text_content; *counter; count++, counter++)
+			;
+	}
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-	if (text_content == NULL)
-		count = 0;
 	if (write(fd, text_content, count) == -1)
 		return (1);
 	close(fd);
