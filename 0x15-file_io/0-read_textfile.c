b50c2a6e49f5f7cfe2a1e08fa93f7a9eaaa57e44
waicharikim <waicharikimani@gmail.com>
2023-07-14 13:38:16 -0700

    betty fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,39 +8,39 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  int fd;
-  char *buf;
-  ssize_t rbytes;
-  ssize_t count;
+	int fd;
+	char *buf;
+	ssize_t rbytes;
+	ssize_t count;
 
-  count = 0;
-  
-  buf = malloc(sizeof(char) * letters);
-  if (!buf)
-    {
-      free(buf);
-      return (0);
-    }
-  if (!filename)
-    {
-    free(buf);
-    return (0);
-    }
-  else
-   fd = open(filename, O_RDONLY);
-  rbytes = read(fd, buf, letters);
-  if (rbytes == -1 || fd == -1)
-    {
-      free(buf);
-      return (0);
-    }
-  count = write(STDOUT_FILENO, buf, rbytes);
-  if (count != rbytes || count == -1)
-    {
-      free(buf);
-      return (0);
-    }
-  free(buf);
-  close(fd);
-  return (count);
+	count = 0;
+
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+	{
+		free(buf);
+		return (0);
+	}
+	if (!filename)
+	{
+		free(buf);
+		return (0);
+	}
+	else
+		fd = open(filename, O_RDONLY);
+	rbytes = read(fd, buf, letters);
+	if (rbytes == -1 || fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	count = write(STDOUT_FILENO, buf, rbytes);
+	if (count != rbytes || count == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	close(fd);
+	return (count);
 }
