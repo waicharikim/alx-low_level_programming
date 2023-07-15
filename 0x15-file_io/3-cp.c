fdf4f9d513e81584f2004fd998797e7946c42666
waicharikim <waicharikimani@gmail.com>
2023-07-14 17:38:59 -0700

    copy file contents

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * main - copies content of a file to another file
+ * @argc: argument count
+ * @argv: pointer to argument list
+ *
+ * Return: 0 for success
+ */
+int main(int argc, char **argv)
+{
+	int fd;
+	int fd2;
+	int nb_read;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+                {
+                    dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+                    exit(98);
+                }
+	nb_read = read(fd, buf, 1024);
+	fd2 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+		if (fd2 == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+		write(fd2, buf, nb_read);
+		if (close(fd) == -1)
+		{
+			dprintf(2, "Error: Can't close fd %d\n", fd);
+			exit(100);
+		}
+		if (close(fd2) == -1)
+		{
+			dprintf(2, "Error: Can't close fd %d\n", fd2);
+			exit(100);
+		}
+		return (1);
+}
